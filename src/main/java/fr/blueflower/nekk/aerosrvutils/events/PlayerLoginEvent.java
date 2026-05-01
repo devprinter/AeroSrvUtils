c36c8571462f3100b4cd60004e41ddde1e62725b
nekk <108535017+iam-nekk@users.noreply.github.com>
2026-05-01 21:03:46 +0200

    changed currency amount

--- src/main/java/fr/blueflower/nekk/aerosrvutils/events/PlayerLoginEvent.java
+++ src/main/java/fr/blueflower/nekk/aerosrvutils/events/PlayerLoginEvent.java
@@ -13,7 +13,10 @@ import org.slf4j.LoggerFactory;
 
 public class PlayerLoginEvent {
     private static final Logger log = LoggerFactory.getLogger(PlayerLoginEvent.class);
-    private final Item currencyToGive = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath("numismatics", "cog"));
+    private final Item CURRENCY_COG = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath("numismatics", "cog"));
+    private final Item CURRENCY_SPROCKET = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath("numismatics", "sprocket"));
+    private final Item CURRENCY_BEVEL = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath("numismatics", "bevel"));
+    private final Item CURRENCY_SPUR = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath("numismatics", "spur"));
 
     @SubscribeEvent
     public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event){
@@ -21,7 +24,21 @@ public class PlayerLoginEvent {
         CompoundTag playerData = player.getPersistentData();
         if (!playerData.getBoolean("receivedCurrency")){
             log.info(String.format("%s (%s) has not received any currency yet!", player.getName().getString(), player.getStringUUID()));
-            player.getInventory().add(new ItemStack(currencyToGive,8));
+
+            // doing it this way is required, otherwise items won't be given properly when inventory is full
+            ItemStack cogs = new ItemStack(CURRENCY_COG,4);
+            ItemStack sprockets = new ItemStack(CURRENCY_SPROCKET,8);
+            ItemStack bevels = new ItemStack(CURRENCY_BEVEL,16);
+            ItemStack spurs = new ItemStack(CURRENCY_SPUR,32);
+
+            // if, when giving the stack, they dont have space for it, theyll drop it instead
+            // apparently this is the recommended way, because if they already had the item in the inventory,
+            // but not enough space to give the full amount, itll throw the remainder
+            // this is useless for us, but im still adding it just in case
+            if (!player.addItem(cogs)) player.drop(cogs, false);
+            if (!player.addItem(sprockets)) player.drop(sprockets, false);
+            if (!player.addItem(bevels)) player.drop(bevels, false);
+            if (!player.addItem(spurs)) player.drop(spurs, false);
             playerData.putBoolean("receivedCurrency", true);
         }
     }
